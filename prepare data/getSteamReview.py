ad3dbe420536802352a80491e48322ad25a7e60b
dapin <dapin1490@gmail.com>
2022-07-16 17:07:59 +0900

    220716 데이터 재수집

--- prepare data/getSteamReview.py
+++ prepare data/getSteamReview.py
@@ -5,48 +5,45 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.options import Options
-import time
 import pandas as pd
-from datetime import datetime
 
 # steam web api doc : https://steamapi.xpaw.me/#IStoreService/GetAppList
 
 # 게임 아이디 csv 불러오기
-games = pd.read_csv("../UROP-game-prediction/prepare data/data/games.csv")
-revw = []
+games = pd.read_csv("../UROP-game-prediction/prepare data/data/games_02.csv")
+games["review"] = None
+revw = ""
 
-# chrome_driver = ChromeDriverManager().install()
-# service = Service(chrome_driver)
+chrome_driver = ChromeDriverManager().install()
+service = Service(chrome_driver)
 
 # reference : https://stackoverflow.com/questions/72758996/selenium-seleniumwire-unknown-error-cannot-determine-loading-status-from-unkn
 # also reference : https://stackoverflow.com/questions/46920243/how-to-configure-chromedriver-to-initiate-chrome-browser-in-headless-mode-throug
-CHROMEDRIVER_PATH = r"C:\Users\dpgbu\.wdm\drivers\chromedriver\win32\103.0.5060.53\chromedriver.exe"
+# CHROMEDRIVER_PATH = r"C:\Users\dpgbu\.wdm\drivers\chromedriver\win32\103.0.5060.53\chromedriver.exe"
 options = Options()
 options.headless = True
-driver = webdriver.Chrome(CHROMEDRIVER_PATH, options=options)
+driver = webdriver.Chrome(service=service, options=options)
 driver.implicitly_wait(1)
 
 steamURL = "https://steamcommunity.com/app/"  # 기본 링크
 reviewLink = "/reviews/?browsefilter=toprated&snr=1_5_100010_&filterLanguage=koreana"
 
-for i in range(0, len(games["appid"])):  # 게임 아이디 수만큼 반복
-    # steam 크롤링
-    revwURL = steamURL + str(games["appid"][i]) + reviewLink  # 게임 아이디로 상점 페이지 링크 생성
-    print("i :", i, ", lasts :", 1000 - i)
-    print("ID :", games["appid"][i])
+total_num_game = len(games["appid"])
 
-    while True:
-        try:
-            driver.get(revwURL)
-            break
-        except:
-            continue
+for i in range(0, total_num_game):  # 게임 아이디 수만큼 반복
+    # steam 크롤링
+    gid = games["appid"][i]
+    revwURL = steamURL + str(gid) + reviewLink  # 게임 아이디로 상점 페이지 링크 생성
+    print(f"i : {i}, lasts : {total_num_game - i}")
+    print("ID :", gid)
+
+    driver.get(revwURL)
 
     # 국가 제한 게임 여부 확인
     try:
         st = "#error_box"
         driver.find_element(By.CSS_SELECTOR, st)  # 이게 되면 게임 정보 수집 불가능
-        revw.append(None)
+        revw = None
         continue
     except NoSuchElementException:
         pass
@@ -56,7 +53,10 @@ for i in range(0, len(games["appid"])):  # 게임 아이디 수만큼 반복
     # 참고 : https://ddang-goguma.tistory.com/35
     # /html/body/div[1]/div[7]/div[5]/div/div[2]/div/div[1]/div[3]/div/a[1]
     try:
-        st = "#age_gate_btn_continue"
+        driver.find_element(By.CLASS_NAME, "agegate_birthday_selector")
+        select = Select(driver.find_element(By.ID, "ageYear"))
+        select.select_by_visible_text("1999")
+        st = "#view_product_page_btn"
         driver.find_element(By.CSS_SELECTOR, st).click()
     except NoSuchElementException:
         pass
@@ -74,50 +74,15 @@ for i in range(0, len(games["appid"])):  # 게임 아이디 수만큼 반복
         res = None
     except:
         res = None
-    revw.append(res)
+    revw = res
     print(res)
     res = None
-
+    
+    games["review"].iloc[i:i + 1] = revw
+    print(games.iloc[i:i + 1, :].transpose())
+    
     print()  # 그냥 줄내림
-    # if i >= 3:
-    #     break
 
-    GRevw = pd.DataFrame({
-        "review": revw
-    })
-
-    # print(GRevw.info())
-
-    gameWRevw = pd.concat([games, GRevw], axis=1)
-    gameWRevw = gameWRevw.reset_index(drop=True)
-    # print(gameWRevw.info())
-    # print(gameWRevw.head())
-
-    gameWRevw.drop('Unnamed: 0', axis=1, inplace=True)
-    # print(gameWRevw.info())
-    # print(gameWRevw.head())
-
-    gameWRevw.to_csv("../UROP-game-prediction/prepare data/data/gameWReview.csv")
+    games.to_csv("../UROP-game-prediction/prepare data/data/gameWReview_02.csv")
     print("progress saved\n")
-
-    if i >= 515:
-        break
 # end of for
-
-GRevw = pd.DataFrame({
-    "review": revw
-})
-
-# print(GRevw.info())
-
-gameWRevw = pd.concat([games, GRevw], axis=1)
-gameWRevw = gameWRevw.reset_index(drop=True)
-# print(gameWRevw.info())
-# print(gameWRevw.head())
-
-gameWRevw.drop('Unnamed: 0', axis=1, inplace=True)
-# print(gameWRevw.info())
-# print(gameWRevw.head())
-
-gameWRevw.to_csv("../UROP-game-prediction/prepare data/data/gameWReview.csv")
-# print("progress saved\n")
